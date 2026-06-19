8c9fb803abd70088710761f281815b914659206a
Deepak <Deepakjangra605@gmail.com>
2026-06-19 21:58:15 +0530

    Initial Setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:bitcoin_ticker_flutter/price_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
   }
 }
